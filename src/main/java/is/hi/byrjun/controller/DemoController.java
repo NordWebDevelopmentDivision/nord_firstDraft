ac561d4cdf1903264633a9aabb1f07e407c85f94
stellarutg <stellarut95@gmail.com>
2017-09-06 17:23:21 +0000

    Verkefni 1

--- src/main/java/is/hi/byrjun/controller/DemoController.java
+++ src/main/java/is/hi/byrjun/controller/DemoController.java
@@ -7,34 +7,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/** Höfundar:
+ *      Kári Snær Kárason (ksk12@hi.is),
+ *      Ólafur Georg Gylfason (ogg4@hi.is),
+ *      Stella Rut Guðmundsdóttir (srg30@hi.is)
+ * Ath. skrifað eftir fyrirlestri hjá Ebbu Þóru 31. ágúst 2017.
+ */
+
 /**
  * Byrjunarcontroller sem stýrir hvað er gert þegar notandi eða viðmót
- * setur inn skipun 
+ * setur inn skipun.
  */
 
 @Controller
 @RequestMapping("/demo") // Request Mapping er gerð fyrir klasann til að slóðin byrji á /demo fyrir allar skipanir 
 public class DemoController {
 
-
-    // Þar sem klasinn hefur slóðina "/demo", er þessi slóð "/demo/page"
-    @RequestMapping("/page")
-    public String demoPage(){
-        return "demo/demo"; // skilar .jsp skrá sem er /webapp/WEB-INF/vefvidmot/demo/demo.jsp
-                            // skoðið application.properties til að sjá hvernig slóðin er sett
-    }
-
-    @RequestMapping("/notandi")
-    public String notandi (Model model) {
-        model.addAttribute("nafn", "Jón Jónsson");
-        return "demo/synaNotandi";
-    }
-
+    /**
+     * Fallið sér um að birta hvadaNotandi.jsp skránna í vafra.
+     * @return .jsp skrá sem er /webapp/WEB-INF/vefvidmot/demo/hvadaNotandi.jsp
+     */
     @RequestMapping("/spyrjaNotanda")
     public String spyrjaNotandi () {
         return "demo/hvadaNotandi";
     }
 
+    /**
+     * Fallið sér um að birta synaNotandi.jsp skránna í vafra.
+     * @param nafn Nafnið sem er slegið inn á vefsíðunni
+     * @param model Módelið sem bæta á nafninu við
+     * @return .jsp skrá sem er /webapp/WEB-INF/vefvidmot/demo/synaNotandi.jsp
+     */
     @RequestMapping(value="/hver", method= RequestMethod.POST)
     public String hver (@RequestParam(value="nafn", required=false) String nafn, ModelMap model) {
         model.addAttribute("nafn", nafn);
--- src/main/webapp/WEB-INF/vefvidmot/demo/synaNotandi.jsp
+++ src/main/webapp/WEB-INF/vefvidmot/demo/synaNotandi.jsp
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 
+<!--
+ Höfundar:
+     Kári Snær Kárason (ksk12@hi.is),
+     Ólafur Georg Gylfason (ogg4@hi.is),
+     Stella Rut Guðmundsdóttir (srg30@hi.is)
+ Ath. skrifað eftir fyrirlestri hjá Ebbu Þóru 31. ágúst 2017.
+ -->
+
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html; charset=UTF-8" %>
